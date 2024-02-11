14597236bec78db33fdba2db1ec4cc9512952141
Pritam Singh <iamcreativepritam@gmail.com>
2024-02-11 16:27:22 +0530

    making footer responsive

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -39,7 +39,7 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
-      <div className="shopcategory-loadnore">
+      <div className="shopcategory-loadmore">
         Explore More
       </div>
     </div>
